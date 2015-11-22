283c7766209ebfb915d02be8933022174bd1dd1b
Zhang LongQi <zhanglongqi@users.noreply.github.com>
2015-11-22 22:56:35 +0800

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,6 @@ struct timeval stop, start;
 
 int main() {
     int i;
-//    int j;
 
     int x[8192], y[8192];
     int a = 1, b = 2, c = 3;
@@ -12,10 +11,7 @@ int main() {
     gettimeofday(&start, NULL);
 
     for (i = 0; i < 8192; i++) {
-//        for (j = 0; j < 8192; j++) {
             y[i] = a * x[i] * x[i] + b * x[i] + c;
-//            y[j] = a * x[j] * x[j] + b * x[j] + c;
-//        }
     }
 
     gettimeofday(&stop, NULL);
