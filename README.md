d9d8db97512b11d5573896fc2062cf7c1a58bae2
longqi <longqi90@gmail.com>
2015-11-24 20:14:34 +0800

    success

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # vectorize_c
+
+If y[] is not used after assigned, this assignment will be optimized also.
+
+`gcc -O2  -ftree-vectorize -ftree-vectorizer-verbose=1 main.c`
+
+Here is the output:
+
+```
+main.c: In function ‘main’:
+main.c:19:3: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘__suseconds_t’ [-Wformat=]
+   printf("%d us\n", stop.tv_usec - start.tv_usec);
+   ^
+
+Analyzing loop at main.c:21
+
+Analyzing loop at main.c:13
+
+
+Vectorizing loop at main.c:13
+
+main.c:13: note: LOOP VECTORIZED.
+main.c:5: note: vectorized 1 loops in function.
+```
--- main.c
+++ main.c
@@ -3,21 +3,24 @@
 struct timeval stop, start;
 
 int main() {
-    int i;
+  int i;
 
-    int x[8192], y[8192];
-    int a = 1, b = 2, c = 3;
+  int x[8192], y[8192];
+  int a = 1, b = 2, c = 3;
 
-    gettimeofday(&start, NULL);
+  gettimeofday(&start, NULL);
 
-    for (i = 0; i < 8192; i++) {
-            y[i] = a * x[i] * x[i] + b * x[i] + c;
-    }
+  for (i = 0; i < 8192; i++) {
+    y[i] = a * x[i] * x[i] + b * x[i] + c;
+  }
 
-    gettimeofday(&stop, NULL);
+  gettimeofday(&stop, NULL);
 
-    printf("%d us\n", stop.tv_usec - start.tv_usec);
-    return 0;
+  printf("%d us\n", stop.tv_usec - start.tv_usec);
+
+  for (i = 0; i < 8192; i++) {
+    printf("%d\r", y[i]);
+  }
+
+  return 0;
 }
-
-
